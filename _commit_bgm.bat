0c73e27e2ced91561f57ed1d0cf087e56ca9b446
story-author <author@example.com>
2026-06-28 17:40:22 +0800

    feat: add custom BGM for story1

---
+++ _commit_bgm.bat
@@ -0,0 +1,6 @@
+cd C:\Users\20442\Documents\Kimi\Workspaces\网站\my-stories
+git add -A
+git commit -m "feat: add custom BGM for story1"
+git log --oneline
+git status
+pause
--- _commit_spa.bat
+++
@@ -1,6 +0,0 @@
-cd C:\Users\20442\Documents\Kimi\Workspaces\网站\my-stories
-git add -A
-git commit -m "feat: seamless SPA chapter navigation"
-git log --oneline
-git status
-pause
